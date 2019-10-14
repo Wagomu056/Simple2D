884c59b3faad39b2934c8ec4cb6b202b4d7c01f9
Wagomu056 <catchballer@gmail.com>
2019-10-14 23:39:46 +0900

    ジャンプ中空中制御追加

--- Assets/Characters/Player/Scripts/Action/ActionJump.cs
+++ Assets/Characters/Player/Scripts/Action/ActionJump.cs
@@ -5,7 +5,12 @@ namespace Action
 public class Jump : State
 {
     StatusWithAnimFlag status;
-    ContactFilter2D filter2D;
+    ContactFilter2D filter2D = new ContactFilter2D();
+
+    float velX = 0.0f;
+
+    const float MaxVelX = 5.0f;
+    const float AddVelX = (MaxVelX / 0.2f);
 
     public Jump(Animator animator, UInput.InputComponet input, Rigidbody2D rigid)
     : base(animator, input, rigid)
@@ -16,10 +21,17 @@ public class Jump : State
     public override void Start()
     {
         rigid.velocity += new Vector2(0.0f, 5.0f);
+        velX = rigid.velocity.x;
         status.Set(StatusWithAnimFlag.Type.Jumping);
     }
 
     public override void Update()
+    {
+        UpdateStatus();
+        UpdateRigid();
+    }
+
+    void UpdateStatus()
     {
         switch(status.Current)
         {
@@ -40,6 +52,18 @@ public class Jump : State
             }
             break;
         }
+    }
+
+    void UpdateRigid()
+    {
+        if (status.Current != StatusWithAnimFlag.Type.Falling)
+        {
+            return;
+        }
+
+        velX += AddVelX * input.GetHorizontal() * Time.deltaTime;
+        velX = Mathf.Clamp(velX, -MaxVelX, MaxVelX);
+        rigid.velocity = new Vector2(velX, rigid.velocity.y);
     }
 
     public override bool ShouldStart()
