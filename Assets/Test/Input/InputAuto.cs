ab4e2e5b1d54f098ef18093e9a7f4e0386307112
Wagomu056 <catchballer@gmail.com>
2019-10-19 16:58:06 +0900

    テスト用自動入力インプット追加

---
+++ Assets/Test/Input/InputAuto.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace UInput
+{
+public class InputAuto : InputComponet
+{
+    float requestHorizontalDir = 0.0f;
+    float requestHorizontalTimeLimit = 0.0f;
+
+    float requestJumpTimeLimit = 0.0f;
+
+    public void RequestHorizontal(float dir, float time)
+    {
+        requestHorizontalDir = dir;
+        requestHorizontalTimeLimit = Time.time + time;
+    }
+
+    public void RequestJump(float time)
+    {
+        requestJumpTimeLimit = Time.time + time;
+    }
+
+    public override float GetHorizontal()
+    {
+        if (Time.time < requestHorizontalTimeLimit)
+        {
+            return requestHorizontalDir;
+        }
+
+        return 0.0f;
+    }
+
+    public override bool GetJump()
+    {
+        return (Time.time <= requestJumpTimeLimit);
+    }
+}
+} // namespace UInput
